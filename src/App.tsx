e4e95f20766978c08f1105fa77ade741ea77ca83
Gelo <christian.sulit@kmc.solutions>
2020-11-05 15:24:04 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles/app.less';
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <p className='font-proxiExtraBold text-kmcOrange text-4xl'>
+        KMC Solutions
+      </p>
+    </div>
+  );
+}
+
+export default App;
