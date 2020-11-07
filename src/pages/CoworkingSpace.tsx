e79e621d8d351db1560b81ff0328238a08f9c40e
joelkmc <jhon.cabagua@kmc.solutions>
2020-11-07 09:25:03 +0800

    Created layout for the components of the page

--- src/pages/CoworkingSpace.tsx
+++ src/pages/CoworkingSpace.tsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import { Layout, Row, Col } from 'antd';
+
+import Navigation from '../components/Navigation';
+import HeroSection from '../components/HeroSection';
+import FeatureSection from '../components/FeatureSection';
+import LocationSection from '../components/LocationSection';
+
+const { Header, Content } = Layout;
 
 export default function CoworkingSpace() {
+
   return (
-    <div>
+    <Layout className="flex flex-col bg-white">
+    
+      {/* Header Layout */}
+      <Header 
+        className='bg-black px-0 h-14' 
+        style={{ position: 'sticky', zIndex: 1, width: '100%' }}
+      >
+        <Row>
+          <Col span={16} className='mx-auto'>
+            <Navigation />
+          </Col>
+        </Row>
+      </Header>
       
-    </div>
+      {/* Main Section Layout */}
+      <Content>
+
+        {/* Hero Section Layout */}
+        <Row>
+          <Col span={16} className='mx-auto'>
+            <HeroSection />
+          </Col>
+        </Row>
+
+        {/* Features Section Layout */}
+        <Row>
+          <Col span={16} className='mx-auto'>
+            <FeatureSection />
+          </Col>
+        </Row>
+        
+        {/* Locations Section Layout */}
+        <Row>
+          <Col span={16} className='mx-auto'>
+            <LocationSection />
+          </Col>
+        </Row>
+
+      </Content>
+    
+    </Layout>
   )
 }
+  
